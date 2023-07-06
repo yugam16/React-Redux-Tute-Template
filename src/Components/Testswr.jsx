61216ded46b32742eeedcf2d2fd4e718569e7848
Yugam Kakkar <106383699+yugam16@users.noreply.github.com>
2023-07-06 16:47:27 +0530

    Update Testswr.jsx

--- src/Components/Testswr.jsx
+++ src/Components/Testswr.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 const fetcher =  async url => await axios.get(url).then(res => res.data);
 
 export default function FetchData() {
-    const {data,error,isLoading} = useSWR('add your url here', fetcher);
+    const {data,error,isLoading} = useSWR('your url here', fetcher);
   return (
    <>
         {isLoading ? <div>data is being loaded...</div> : 
